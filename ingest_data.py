9c0d64752e04dab8b683b065bb20353598fa4d36
io8ex <48648246+io8ex@users.noreply.github.com>
2022-03-31 00:37:06 +0200

    Add files via upload

--- ingest_data.py
+++ ingest_data.py
@@ -5,28 +5,32 @@ import urllib.request
 from kafka import KafkaProducer
 import ast
 
-producer = KafkaProducer(bootstrap_servers="localhost:9092")
-topic_name = "stocks_test"
 
-i = 0
+def start_producer(stock_name, time_sleep=1):
+	producer = KafkaProducer(bootstrap_servers="localhost:9092")
 
-while True:
-	ticker = yf.Ticker("MSFT")
+	i = 0
 
-	"""if i == 0:
-					message_df = ticker.history(period="1d", interval="1m")
-				else:"""
-	message_df = ticker.history(period="1d", interval="1m").iloc[-1]
+	while True:
+		ticker = yf.Ticker(stock_name)
 
-	message = message_df.to_json(orient="index").encode("utf-8")
-	closing_stock = ast.literal_eval(message.decode('UTF-8'))['Close']
-	producer.send(topic_name, message)
-	print('----------------------')
-	print(f"Sending stocks info to topic: {topic_name} at time {time.time()}")
-	print(message)
+		message_df = ticker.history(period="1d", interval="1m").iloc[-1]
 
-	time.sleep(1)
-    
+		message = message_df.to_json(orient="index").encode("utf-8")
+		closing_stock = ast.literal_eval(message.decode('UTF-8'))['Close']
+		producer.send(stock_name, message)
+		print('----------------------')
+		print(f"Sending stocks info to topic: {stock_name} at time {time.time()}")
+		print(message)
+
+		time.sleep(time_sleep)
+
+
+
+
+
+
+
 
 
 
