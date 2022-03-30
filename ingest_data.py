89282daa0b56007655be2033c57518ea7d1c2097
Yasmine Guemouria <yassmineguem@gmail.com>
2022-03-30 23:36:23 +0200

    tried putting ingestion in a function but no

--- ingest_data.py
+++ ingest_data.py
@@ -8,14 +8,9 @@ import ast
 producer = KafkaProducer(bootstrap_servers="localhost:9092")
 topic_name = "stocks_test"
 
-i = 0
-
 while True:
 	ticker = yf.Ticker("MSFT")
 
-	"""if i == 0:
-					message_df = ticker.history(period="1d", interval="1m")
-				else:"""
 	message_df = ticker.history(period="1d", interval="1m").iloc[-1]
 
 	message = message_df.to_json(orient="index").encode("utf-8")
@@ -26,7 +21,6 @@ while True:
 	print(message)
 
 	time.sleep(5)
-    
 
 
 
